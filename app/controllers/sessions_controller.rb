bf02df326e6c703c0dc6197a27487f7b1857b72f
salmannausher <sallu4548@gmail.com>
2022-06-14 17:10:52 +0500

    byebug removed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
   
     def create
       @user = User.find_by(email: params[:email])
-      byebug
       if @user && @user.authenticate(params[:password])
         session[:user_id] = @user.id
         redirect_to dashboard_path
